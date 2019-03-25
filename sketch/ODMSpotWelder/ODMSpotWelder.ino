1bd5c4d2406f07e1ca075ac480e6e9ede397b73f
mcecchi <mauro.cecchi@met-informatica.it>
2019-03-25 22:27:02 +0100

    Added OneButton library

--- sketch/ODMSpotWelder/ODMSpotWelder.ino
+++ sketch/ODMSpotWelder/ODMSpotWelder.ino
@@ -2,8 +2,8 @@
 #include <math.h> // standard library used for temperature calculations
 //#include <LiquidCrystal_I2C.h> // for I2C, you need the new library installed (the link is just below)
 #include <LiquidCrystal.h>
-
-#define DEBUG_MODE
+#include "OneButton.h"
+//#define DEBUG_MODE
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int menupin = 7;
@@ -23,33 +23,48 @@ int weldtime = 0;
 int cutofftemp = 80;
 int resumetemp = 40;
 
+OneButton menuButton(menupin, true, true);
+OneButton weldButton(weldbuttonpin, true, true);
+
 void setup() {
-  pinMode(switchpin, OUTPUT);
-  pinMode(weldbuttonpin, INPUT_PULLUP);
-  pinMode(menupin, INPUT_PULLUP);
-  pinMode(thermpin, INPUT);
+#ifdef DEBUG_MODE
   Serial.begin(9600);
+#endif
+  pinMode(switchpin, OUTPUT);
+  //pinMode(weldbuttonpin, INPUT_PULLUP);
+  //pinMode(menupin, INPUT_PULLUP);
+  pinMode(thermpin, INPUT);
   maxweldpulse = (maxweldpulse - weldtimeincrement);
   weldtime = weldtimeincrement;
+  menuButton.attachClick(menuchange);
+  menuButton.attachDuringLongPress(menuchange);
+  menuButton.setDebounceTicks(20); // default=50
+  menuButton.setClickTicks(100); // default=600
+  menuButton.setPressTicks(200); // default=1000
+  weldButton.attachClick(weld);
+  weldButton.setDebounceTicks(20); // default=50
+  weldButton.setClickTicks(100); // default=600
   lcd.begin(16, 2);
   menudisplay();
 }
 
 void loop() {
 #ifdef DEBUG_MODE
-  Serial.println(Thermistor(analogRead(thermpin)));
+  Serial.println(ThermistorRead(thermpin));
 #endif
-  if (round(Thermistor(analogRead(thermpin))) >= cutofftemp)  {
+  if (ThermistorRead(thermpin) >= cutofftemp)  {
     cooldown();
   }
-  if (digitalRead(menupin) == LOW)  {
-    delay(debouncepause);
-    menuchange();
-  }
-  if (digitalRead(weldbuttonpin) == LOW)  {
-    delay(debouncepause);
-    weld();
-  }
+  //if (digitalRead(menupin) == LOW)  {
+  //  delay(debouncepause);
+  //  menuchange();
+  //}
+  menuButton.tick();
+  //if (digitalRead(weldbuttonpin) == LOW)  {
+  //  delay(debouncepause);
+  //  weld();
+  //}
+  weldButton.tick();
   menudisplay();
 }
 
@@ -66,15 +81,15 @@ void menuchange() {
 void menudisplay()  {
   //lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Weld time |Temp.");
+  lcd.print("Weld time|Temp.");
   lcd.setCursor(0, 1);
   lcd.print(weldtime);
-  lcd.print(" ms   ");
-  lcd.setCursor(10, 1);
+  lcd.print(" ms  ");
+  lcd.setCursor(9, 1);
   lcd.print("|");
-  lcd.print(round(Thermistor(analogRead(thermpin))));
-  lcd.print("C   ");
-  delay (100);
+  lcd.print(ThermistorRead(thermpin));
+  lcd.print("C ");
+  //delay (100);
 }
 
 void weld() {
@@ -94,20 +109,35 @@ void weld() {
 
 void cooldown() {
   lcd.clear();
-  while (round(Thermistor(analogRead(thermpin))) > resumetemp)  {
+  while (ThermistorRead(thermpin) > resumetemp)  {
     lcd.setCursor(0, 0);
-    lcd.print("Cooling...|Temp.");
-    lcd.setCursor(10, 1);
+    lcd.print("Cooling..|Temp.");
+    lcd.setCursor(9, 1);
     lcd.print("|");
-    lcd.print(round(Thermistor(analogRead(thermpin))));
-    lcd.print("C   ");
-    delay(500);
+    lcd.print(ThermistorRead(thermpin));
+    lcd.print("C ");
+    delay(10);
   }
   menudisplay();
 }
 
-double Thermistor1(int RawADC) {
-  double Temp;
+float ThermistorRead(int pin)	{
+#define NUMSAMPLES 5
+  byte i;
+  int sample;
+  float average = 0.0;
+ 
+  for (i=0; i<NUMSAMPLES; i++) {
+	sample = analogRead(pin);
+	average += sample;
+	delay(10);
+  }
+  average /= NUMSAMPLES;
+  return ADC2Temp2(average);
+}
+
+float ADC2Temp(int RawADC) {
+  float Temp;
   // Temp = log(100000.0 * ((1023.0 / RawADC - 1)));
   Temp = log(100000.0 / (1023.0 / RawADC - 1)); // for pull-up configuration
   Temp = 1.0 / (0.0008271111 + (0.000208802 + (0.000000080592 * Temp * Temp )) * Temp );
@@ -115,8 +145,8 @@ double Thermistor1(int RawADC) {
   return Temp;
 }
 
-double Thermistor(int RawADC) {
-  double Temp;
+float ADC2Temp1(int RawADC) {
+  float Temp;
   // Temp = log(10000.0 * ((1023.0 / RawADC - 1)));
   Temp = log(10000.0 / (1023.0 / RawADC - 1)); // for pull-up configuration
   Temp = 1.0 / (0.001129241 + (0.0002341077 + (0.00000008775468 * Temp * Temp )) * Temp );
@@ -124,8 +154,8 @@ double Thermistor(int RawADC) {
   return Temp;
 }
 
-double Thermistor2(int RawADC)	{
-  double Temp;
+float ADC2Temp2(int RawADC)	{
+  float Temp;
   Temp = RawADC / (1023.00 - RawADC);	// (R/Ro)
   Temp = log(Temp);            			// ln(R/Ro)
   Temp /= 3950.0;         			    // 1/B * ln(R/Ro)
